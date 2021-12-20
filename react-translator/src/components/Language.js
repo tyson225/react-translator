b82a5e9fee02b30f954634e377ef98f861c328c7
Tyson McDonald <tyson22509@hotmail.com>
2021-12-20 18:32:35 -0500

    cleaned up starter create-react-app files and started working on the api integration

---
+++ react-translator/src/components/Language.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const LANGUAGES = [
+    { label: "Arabic", value: 'ar'},
+    { label: "Bengali", value: 'be'},
+    { label: "French", value: 'fr'},
+    { label: "Hindi", value: 'hi'},
+    { label: "Indonesian", value: 'id'},
+    { label: "Portuguese", value: 'pt'},
+    { label: "Simplified Chinese", value: 'zh-CN'},
+    { label: "Spanish", value: 'es'},
+    { label: "Russian", value: 'ru'},
+]
---
+++ react-translator/src/components/Translate.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Translate = async (input, languageCode, cancelToken) => {
+    "AIzaSyB8wafIGpWbJtnUSNB9mEU3Ssw6lNXHWk0"
+}
